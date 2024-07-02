d823cb184ffbbd2b7273d4d111cdaf59a6c47a5a
wuyilong <wuyilong@doctortech.com.cn>
2024-07-02 17:13:57 +0800

    redis-init

---
+++ springboot-redis/src/main/java/com/wyl/redis/RedisApplication.java
@@ -0,0 +1,18 @@
+package com.wyl.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @Description
+ * @Author WuYiLong
+ * @Date 2024/7/2 9:56
+ */
+@SpringBootApplication
+public class RedisApplication {
+
+    public static void main(String[] args) {
+       SpringApplication.run(RedisApplication.class);
+    }
+}
